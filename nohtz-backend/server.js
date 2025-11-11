e57da5aef80a1ada02539e202e7fe5e7e0772405
WDgaste <davidshoimer@gmail.com>
2025-11-11 18:29:34 -0500

    finalized placeholder sub pages

--- nohtz-backend/server.js
+++ nohtz-backend/server.js
@@ -5,6 +5,7 @@ import {startupServices} from "./services.js";
 
 const app = express();
 app.use(express.json());
+app.use(express.static("public"));
 
 //DB Connection
 const db = await mysql.createConnection({
