1f3faf50581c5e8d306d52af50d0e9b641d2a188
WDgaste <davidshoimer@gmail.com>
2025-12-16 16:32:22 -0500

    temp

--- nohtz-backend/server.js
+++ nohtz-backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import mysql from "mysql2/promise";
 import {startupServices} from "./services.js";
 
+import path from "path";
 
 const app = express();
 app.use(express.json());
@@ -15,13 +16,13 @@ password: "Logman718",
 database: "notes_app",
 });
 
-//Test
-app.get("/", (req, res) => {
-    res.send("Backend is running");
-});
-
 //Services
 startupServices(app, db);
 
 //Server Start Message
 app.listen(3000, () => console.log("Server running on http://localhost:3000"));
+
+//Test
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + "/../nohtz-frontend/index.html"));
+});
